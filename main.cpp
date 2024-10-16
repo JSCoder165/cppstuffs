7b1e10e907b535f2779b18433c488331f7b45728
Dhruv <80367377+JSCoder165@users.noreply.github.com>
2024-10-15 21:07:52 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,21 @@
 int main(){
     std::cout <<"Hello World!\n";
 
+    int x;
+
+    std::cout <<"Enter a number!\n";
+    std::cin >> x; // how to get input from user
+
+    for (int i = 0; i <= x; i++) {
+    std::cout << i << "\n";
+    }
+
+
+    /*
     std::cin.clear(); // reset any error flags
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find an enter character
     std::cin.get(); // get one more char from the user
+    */
+
     return 0;
 }
